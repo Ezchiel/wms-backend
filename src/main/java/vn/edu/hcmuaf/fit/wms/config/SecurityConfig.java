7223b0dfb6f2f3f054c254f80d123109db7be6f3
Ezchiel <hoangquocluat88@gmail.com>
2026-05-25 13:29:28 +0700

    Add swagger

--- src/main/java/vn/edu/hcmuaf/fit/wms/config/SecurityConfig.java
+++ src/main/java/vn/edu/hcmuaf/fit/wms/config/SecurityConfig.java
@@ -44,7 +44,17 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+
+                        // Public
                         .requestMatchers("/api/auth/**").permitAll()
+
+                        // Swagger
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
